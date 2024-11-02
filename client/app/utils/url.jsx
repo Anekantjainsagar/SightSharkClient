54f80081cd918343d31d001a35d3e537a25a64d4
AshisMaity <maity.ashis651@gmail.com>
2024-11-02 16:30:27 +0530

    docker image

--- client/app/utils/url.jsx
+++ client/app/utils/url.jsx
@@ -1,3 +1,6 @@
-export const BACKEND_URI = "https://sightshark-agency-portal.onrender.com";
-export const ADMIN_BACKEND_URI =
+export const BACKEND_URI = process.env.NEXT_PUBLIC_BACKEND_URI;
+export const ADMIN_BACKEND_URI = process.env.NEXT_PUBLIC_ADMIN_BACKEND_URI;
   "https://admin-portal-backend-118842008879.asia-south1.run.app";
+// export const BACKEND_URI = "https://sightshark-agency-portal.onrender.com";
+// export const ADMIN_BACKEND_URI =
+//   "https://admin-portal-backend-118842008879.asia-south1.run.app";
